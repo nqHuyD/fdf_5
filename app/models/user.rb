3cf7dfff0886830d68383de2781c498fb7a42603
nguyen.quang.huyd <FRAMGIA\nguyen.quang.huyd@b121623-pc.framgia.com>
2018-12-18 08:01:26 +0700

    Basic Sign Up and Sign In Feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,45 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
+  before_save :downcase_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name, presence: true,
+    length: {maximum: Settings.validates.user.name.length.maximum}
+  validates :email, presence: true,
+    length: {maximum: Settings.validates.user.email.length.maximum},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true,
+    length: {minimum: Settings.validates.user.password.length.minimum},
+    allow_nil: true
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  class << self
+    def digest string
+      min_cost = ActiveModel::SecurePassword.min_cost
+      cost = min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
